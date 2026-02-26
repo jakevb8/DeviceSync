be46a618cd51f0bba2143c6af965687e6cc38c79
Jake Van Buren <jakevb8@gmail.com>
2026-02-26 18:17:55 -0500

    fix: target API level 35 for Google Play compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "me.jakev.devicesync"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "me.jakev.devicesync"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
